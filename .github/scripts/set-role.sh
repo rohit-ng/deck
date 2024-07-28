25a37633fe5fa4ac834defb298a4f1df9539ff26
rohit-ng <rohit.int24@infraspec.dev>
2024-07-28 19:17:03 +0530

    change dir structure

---
+++ .github/scripts/set-role.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+root_dir=$(echo "$1" | cut -d '/' -f 1)
+
+case "$root_dir" in
+  dev)
+    ROLE_ARN=${DEV_GH_ROLE}
+    ;;
+    echo "Unknown account $root_dir"
+    exit 1
+    ;;
+esac
+
+echo "Assuming role $ROLE_ARN for account $root_dir"
+echo "role_arn=$ROLE_ARN" >> $GITHUB_OUTPUT
