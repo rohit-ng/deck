a92c27ebe1912a6810afdd774d4fd947e43cc7a2
rohit-ng <rohit.int24@infraspec.dev>
2024-07-28 19:57:54 +0530

    change dir structure

--- .github/scripts/set-role.sh
+++ .github/scripts/set-role.sh
@@ -2,31 +2,25 @@
 
 # Check if input is provided
 if [ -z "$1" ]; then
-  echo "No input provided"
-  exit 1
+    echo "No input provided"
+    exit 1
 fi
 
 # Parse the environment from the input
-environments=$(echo $1 | jq -r '.[] | split("/") | .[1]' | sort | uniq)
-
-# Check if jq parsing was successful
-if [ -z "$environments" ]; then
-  echo "Failed to parse environment"
-  exit 1
-fi
+environment=$(echo $changed_dirs | jq -r '.[] | split("/") | .[0]' | sort | uniq)
 
 # Determine the role ARN based on the environment
 for environment in $environments; do
-  case "$environment" in
+    case "$environment" in
     dev)
-      ROLE_ARN=${DEV_GH_ROLE}
-      ;;
+        ROLE_ARN=${DEV_GH_ROLE}
+        ;;
     *)
-      echo "Unknown environment $environment"
-      exit 1
-      ;;
-  esac
+        echo "Unknown environment $environment"
+        exit 1
+        ;;
+    esac
 done
 
-echo "Assuming role $ROLE_ARN for account $environments"
-echo "role_arn=$ROLE_ARN" >> $GITHUB_OUTPUT
+echo "Assuming role $ROLE_ARN for account $environment"
+echo "role_arn=$ROLE_ARN" >>$GITHUB_OUTPUT
